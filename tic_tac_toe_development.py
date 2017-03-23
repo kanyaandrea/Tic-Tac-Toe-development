bc62e8ab8df1210bdcef907a94ff6a73a234c8b0
Selmeczi Tamas <dizsit@gmail.com>
2017-03-23 21:55:42 +0100

    Final version

--- tic_tac_toe_development.py
+++ tic_tac_toe_development.py
@@ -26,11 +26,12 @@ def welcome_screen():
     print("        ║ ╦╠═╣║║║║╣          ")
     print("        ╚═╝╩ ╩╩ ╩╚═╝         ")
     print("\n\n\n")
-    time.sleep(2)
+    # time.sleep(1)
+    input("Press Enter to continue...")
 
 
 def player_input():
-    os.system("clear")
+    os.system('clear')
     marker = ""
     while not (marker == "X" or marker == "O"):
         marker = input(player_names[0] + " do you want to be X or O?  ").upper()
@@ -42,7 +43,8 @@ def player_input():
 
 
 def display_board(board):
-    os.system('clear') 
+    os.system('clear')
+    print("Please use the numpad to navigate your marker on the board.")
     print("\n"*2)
     print(("    |     |    ").center(50, " "))
     print((" " + board[7] + "  |  " + board[8] + "  |  " + board[9] + " ").center(50, " "))
@@ -86,12 +88,12 @@ def win_check(board, mark):
 def choose_first():
     os.system("clear")
     if random.randint(0, 1) == 0:
-        print(("\n" * 9 + player_names[0] + " starts the game now!!!").upper())
-        time.sleep(2)
+        print(("\n" * 9 + player_names[0] + " starts the game now!!!"))
+        time.sleep(1)
         return player_names[0]
     else:
-        print(("\n" * 9 + player_names[1] + " starts the game now!!!").upper())
-        time.sleep(2)
+        print(("\n" * 9 + player_names[1] + " starts the game now!!!"))
+        time.sleep(1)
         return player_names[1]
 
 
@@ -112,24 +114,34 @@ def full_board_check(board):
 def player_choice(board):
     position = ""
     while position not in "1 2 3 4 5 6 7 8 9".split() or not space_check(board, int(position)):
-        position = input("Please enter your mark's position: (1-9)  ")
+        position = input("Please enter your marker's position: (1-9)  ")
     return int(position)
 
 
-def main():
-    turn = choose_first() 
+def replay():
+    return input("Do you want to play again? Please Press Y/N  ").upper().startswith("Y")
+
+
+welcome_screen()
+player_names = ask_player_name()
+while True:
+    player_marker_choice = player_input()
+    turn = choose_first()
     game_on = True
     the_board = [" "] * 10
+
     while game_on is True:
-        if turn == player_names[0]: 
+        if turn == player_names[0]:
             display_board(the_board)
-            print(player_names[0], ":")
+            print(player_names[0] + ", it's your turn!")
             position = player_choice(the_board)
             place_marker(the_board, position, player_marker_choice[0])
+
             if win_check(the_board, player_marker_choice[0]) is True:
                 display_board(the_board)
                 print("Congratulation " + player_names[0] + "!!! You won the game!")
                 game_on = False
+
             else:
                 if full_board_check(the_board):
                     display_board(the_board)
@@ -137,10 +149,11 @@ def main():
                     break
                 else:
                     turn = player_names[1]
+
         else:
             turn = player_names[1]
             display_board(the_board)
-            print(player_names[1], ":")
+            print(player_names[1] + ", it's your turn!")
             position = player_choice(the_board)
             place_marker(the_board, position, player_marker_choice[1])
 
@@ -148,21 +161,15 @@ def main():
                 display_board(the_board)
                 print("Congratulation " + player_names[1] + "!!! You won the game!")
                 game_on = False
+
             else:
                 if full_board_check(the_board):
                     display_board(the_board)
                     print("This game is draw!")
                     break
+
                 else:
                     turn = player_names[0]
 
-
-welcome_screen()
-player_names = ask_player_name()  
-player_marker_choice = player_input() 
-new_game = "y"
-while new_game == "y":
-    main()
-    new_game = input("Do you want to play a new game? (write y to continue, or press any key to exit)" )
-exit()
-
+    if replay() is False:
+        break
